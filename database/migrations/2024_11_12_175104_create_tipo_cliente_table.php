55135d39ed9ddddcb9233fe816c16480d5bf17a4
MariolyTintaya <tintayaramosmarioly@gmail.com>
2024-11-12 13:51:35 -0400

    Agregando las migraciones

---
+++ database/migrations/2024_11_12_175104_create_tipo_cliente_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tipo_cliente', function (Blueprint $table) {
+            $table->integer('id_tipo', true)->unique('id_tipo_unique');
+            $table->string('descripcion', 45)->nullable();
+
+            $table->primary(['id_tipo']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tipo_cliente');
+    }
+};
